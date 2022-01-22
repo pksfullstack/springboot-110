bd22eb8e197c4587003175626f2090ec9b91bae1
pksfullstack <pksfullstack@gmail.com>
2022-01-22 17:35:42 +0530

    Initial commit

---
+++ src/main/java/com/pks/HelloWorldResource.java
@@ -0,0 +1,14 @@
+package com.pks;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldResource {
+	
+	//http://localhost:8080/springboot-110/message
+	@GetMapping("/message")
+	public String welcomeMessage() {
+		return "Welcome to Sprint Boot V1.";
+	}
+}
