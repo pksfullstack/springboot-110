8d40c01731738c983411de373c4f0dc7d22d2d41
pksfullstack <pksfullstack@gmail.com>
2022-02-12 18:13:42 +0530

    Added hostname

--- src/main/java/com/pks/HelloWorldResource.java
+++ src/main/java/com/pks/HelloWorldResource.java
@@ -1,15 +1,23 @@
 package com.pks;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldResource {
 	
-	// - local  - http://localhost:8080/springboot-110/message
+	// - local  - http://localhost:8080/springboot-110/message or http://localhost:8080/message
 	// - docker - http://localhost:8085/message
 	@GetMapping("/message")
 	public String welcomeMessage() {
-		return "Welcome to Sprint Boot V 8.";
+		try {
+			return "Welcome to Sprint Boot V 9 : " + InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		return "error";
 	}
 }
